2ea3cc82f43eed3a3cd5f7b475d82e16f777cab0
Kowshik <koushikpn071994@gmail.com>
2020-09-23 11:30:53 -0700

    Updated database accroding to model and insert values to the database

--- src/react-tracks/app/tracks/models.py
+++ src/react-tracks/app/tracks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+#Track will be a table in the database and title, description,url, created_at will be columns of the table.
+
+class Track(models.Model):
+    #id will be automatically be created by django.
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True) #description can be None.
+    url = models.URLField()  #url to access the track
+    created_at = models.DateTimeField(auto_now_add=True) #auto_now_add uses current date and time.
